41716bf72d25101cd350d1d40e44fe52c9841ff3
Prathmesh Sadake <prathmeshsadake@gmail.com>
2023-11-23 20:59:59 +0530

    Revert "Installed trpc libraries for next.js"
    This reverts commit 129ec306f5e7f89fb6cfd3a99b3773da23e98231.

--- src/app/api/auth/[kindeAuth]/route.ts
+++ src/app/api/auth/[kindeAuth]/route.ts
@@ -1,10 +1,6 @@
 import { handleAuth } from "@kinde-oss/kinde-auth-nextjs/server";
-import { NextApiRequest } from "next";
 
-export async function GET(
-  request: NextApiRequest,
-  { params }: any
-) {
+export async function GET(request: object | undefined, { params }: any) {
   const endpoint = params.kindeAuth;
   return handleAuth(request, endpoint);
 }
