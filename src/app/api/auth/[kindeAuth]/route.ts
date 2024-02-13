a544d7ffb691757d8b6cf9f233d2a993fc342829
Prathmesh Sadake <prathmeshsadake@gmail.com>
2024-02-13 13:27:30 +0530

    Delete unused files and update configuration files

--- src/app/api/auth/[kindeAuth]/route.ts
+++
@@ -1,6 +0,0 @@
-import { handleAuth } from "@kinde-oss/kinde-auth-nextjs/server";
-
-export async function GET(request: object | undefined, { params }: any) {
-  const endpoint = params.kindeAuth;
-  return handleAuth(request, endpoint);
-}
--- src/components/PdfRenderer.tsx
+++
@@ -1,7 +0,0 @@
-import React from "react";
-
-const PdfRenderer = () => {
-  return <div>PdfRenderer</div>;
-};
-
-export default PdfRenderer;
