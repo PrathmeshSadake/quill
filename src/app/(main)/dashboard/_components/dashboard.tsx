5b156cb4e4fcec3590c8a602d1a3a271a6a1125e
Prathmesh Sadake <prathmeshsadake@gmail.com>
2024-02-14 02:19:34 +0530

    Add date-fns library and implement file deletion functionality

--- src/app/(main)/dashboard/_components/dashboard.tsx
+++ src/app/(main)/dashboard/_components/dashboard.tsx
@@ -1,15 +1,42 @@
 "use client";
 import axios from "axios";
-import { Ghost } from "lucide-react";
+import { Ghost, Loader2, MessageSquare, Plus, Trash } from "lucide-react";
 import Skeleton from "react-loading-skeleton";
-import { useQuery } from "@tanstack/react-query";
-
+import { useMutation, useQuery, useQueryClient } from "@tanstack/react-query";
+import { format } from "date-fns";
 import UploadButton from "./upload-button";
+import { File } from "@prisma/client";
+import Link from "next/link";
+import { useState } from "react";
+import { Button } from "@/components/ui/button";
 
 const Dashboard = () => {
+  const queryClient = useQueryClient();
+  const [currentlyDeletingFile, setCurrentlyDeletingFile] = useState<
+    string | null
+  >(null);
   const { data, error, isLoading } = useQuery({
     queryKey: ["files"],
     queryFn: async () => await axios.get("/api/files"),
+  });
+
+  const { mutate: deleteFile } = useMutation({
+    mutationFn: ({ id }) => {
+      return axios.delete("/api/files", {
+        data: {
+          id,
+        },
+      });
+    },
+    onSuccess: () => {
+      queryClient.invalidateQueries({ queryKey: ["files"] });
+    },
+    onMutate: ({ id }: { id: string }) => {
+      setCurrentlyDeletingFile(id);
+    },
+    onSettled: () => {
+      setCurrentlyDeletingFile(null);
+    },
   });
 
   return (
@@ -20,8 +47,61 @@ const Dashboard = () => {
       </div>
       {/* Display all user files */}
       <div>
-        {data?.data.files && data?.data.files.length ? (
-          <div></div>
+        {data?.data && data?.data.length > 0 ? (
+          <ul className='mt-8 grid grid-cols-1 gap-6 divide-y divide-zinc-200 md:grid-cols-2 lg:grid-cols-3'>
+            {data?.data
+              .sort(
+                (a: File, b: File) =>
+                  new Date(b.createdAt).getTime() -
+                  new Date(a.createdAt).getTime()
+              )
+              .map((file: File) => (
+                <li
+                  key={file.id}
+                  className='col-span-1 divide-y divide-gray-200 bg-white shadow hover:shadow-lg transition'
+                >
+                  <Link
+                    href={`/dashboard/${file.id}`}
+                    className='flex flex-col gap-2'
+                  >
+                    <div className='pt-6 px-6 flex w-full items-center justify-between space-x-6'>
+                      <div className='h-10 w-10 flex-shrink-0 rounded-full bg-gradient-to-r from-cyan-500 to-blue-500' />
+                      <div className='flex-1 truncate'>
+                        <div className='flex items-center space-x-3'>
+                          <h3 className='truncate text-lg font-medium text-zinc-900'>
+                            {file.name}
+                          </h3>
+                        </div>
+                      </div>
+                    </div>
+                  </Link>
+                  <div className='px-6 mt-4 grid grid-cols-3 place-items-center py-2 gap-6 text-xs text-zinc-500'>
+                    <div className='flex items-center gap-2'>
+                      <Plus className='h-4 w-4' />
+                      {format(new Date(file.createdAt), "MMM yyyy")}
+                    </div>
+
+                    <div className='flex items-center gap-2'>
+                      <MessageSquare className='h-4 w-4' />
+                      mocked
+                    </div>
+
+                    <Button
+                      onClick={() => deleteFile({ id: file.id })}
+                      size='sm'
+                      className='w-full'
+                      variant='destructive'
+                    >
+                      {currentlyDeletingFile === file.id ? (
+                        <Loader2 className='h-4 w-4 animate-spin' />
+                      ) : (
+                        <Trash className='h-4 w-4' />
+                      )}
+                    </Button>
+                  </div>
+                </li>
+              ))}
+          </ul>
         ) : isLoading ? (
           <Skeleton height={100} className='my-2' count={4} />
         ) : (
--- src/app/api/files/route.ts
+++ src/app/api/files/route.ts
@@ -23,3 +23,47 @@ export async function GET() {
     return new NextResponse("Internal Server Error", { status: 500 });
   }
 }
+
+export async function DELETE(req: Request, res: Response) {
+  try {
+    const user = await currentUser();
+    if (!user || !user.id) {
+      throw new Error("Unauthorized", {
+        cause: 403,
+      });
+    }
+
+    const body = await req.json();
+    const { id } = body;
+
+    if (!id) {
+      throw new Error("Bad Request", {
+        cause: 400,
+      });
+    }
+
+    const file = await db.file.findFirst({
+      where: {
+        id,
+        userId: user.id,
+      },
+    });
+
+    if (!file) {
+      throw new Error("Not Found", {
+        cause: 404,
+      });
+    }
+
+    await db.file.delete({
+      where: {
+        id,
+      },
+    });
+
+    return new NextResponse("OK", { status: 200 });
+  } catch (error) {
+    console.log(error);
+    return new NextResponse("Internal Server Error", { status: 500 });
+  }
+}
